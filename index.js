27bb3ddefa54c798d15627c0e1865736ff76a0b5
Branden Seaberry <brandenseaberry@gmail.com>
2024-09-29 14:37:59 -0500

    Display Logic

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 let countdown = 30;
 let timerElement = document.getElementById('timer');
-let countDownTimer;
-let startButton = document.getElementById('start-btn');
-let prompt = document.getElementById('prompt');
-let clearButton = document.getElementById('clear-btn');
+const startButton = document.getElementById('start-btn');
+const clearButton = document.getElementById('clear-btn');
+let prompt = document.getElementById('start-banner');
+let input = document.getElementsByTagName('input').value;
+const answer = parseInt(input);
 let minValue = 15;
 let maxValue = 50;
 let questionNumber = 1;
+let countDownTimer;
 
 //Layout Game Logic
 function handleGame() {
-    let numberOne = Math.floor(Math.random() * 
+    const numberOne = Math.floor(Math.random() * 
       (maxValue - minValue + 1)) + minValue;
     
-    let numberTwo = Math.floor(Math.random() * 
+    const numberTwo = Math.floor(Math.random() * 
       (maxValue - minValue + 1)) + minValue;
     
     const solution = numberOne + numberTwo;
     console.log(solution);
     prompt.innerHTML = 
-      `<p>${questionNumber}: ${numberOne} + ${numberTwo}</p>
-      <input type="text" oninput="input === solution ? 
-      alert('try again') : nextProblem()">
+      `<p style="font-size: 20px">${questionNumber}: ${numberOne} + ${numberTwo}</p>
+      <input type="text" oninput="${answer} === ${solution} ? 
+      alert('try again') : nextProblem()"
+      placeholder="Answer">
       </input>`;
   }
 
@@ -45,6 +48,7 @@ startButton.onclick = function () {
     countdown = 30;
     timerElement.innerText = countdown;
     alert('Game Over');
+    prompt.innerHTML = ` <p id ='start-banner'>Press Start</p>`
   }
 }, 1000);
 }
@@ -56,8 +60,17 @@ startButton.onclick = function () {
     countdown = 30;
     timerElement.innerText = countdown;
     alert('Game Over');
+    prompt.innerHTML = ` <p id ='start-banner'>Press Start</p>`
    }
  }
 
 
 //Create a function for evaluating the user input.
+function nextProblem() {
+  questionNumber++;
+  //Increases values after question 5.
+  if(questionNumber > 5) {
+      minValue = 50;
+      maxValue = 75;
+  }
+}

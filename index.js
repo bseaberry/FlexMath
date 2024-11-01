c3d34710d75dac4b199a8c24092f13659533db9d
Branden Seaberry <brandenseaberry@gmail.com>
2024-11-01 18:22:37 -0500

    Updating Number Variables

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ let timerElement = document.getElementById('timer');
 const startButton = document.getElementById('start-btn');
 const clearButton = document.getElementById('clear-btn');
 let prompt = document.getElementById('start-banner');
-let minValue = 15;
-let maxValue = 50;
+let minValue = 2;
+let maxValue = 10;
 let questionNumber = 1;
 let countDownTimer;
+
 let numberOne = Math.floor(Math.random() *
-  (maxValue - minValue + 1)) + minValue;
+(maxValue - minValue + 1)) + minValue;
 
 let numberTwo = Math.floor(Math.random() *
-  (maxValue - minValue + 1)) + minValue;
+(maxValue - minValue + 1)) + minValue;
 
-const solution = numberOne + numberTwo;
+let solution = numberOne + numberTwo;
 
 //Layout Game Logic
 function handleGame() {
-
   console.log(solution);
   prompt.innerHTML =
-    `<p style="font-size: 20px">${questionNumber}: ${numberOne} + ${numberTwo}</p>
+    `<p style="font-size: 20px" id="question">${questionNumber}: ${numberOne} + ${numberTwo}</p>
       <input type="text"
       placeholder="Answer">
       <button id='submit-btn' onclick="processAnswer()">Submit</button>
       </input>`;
 
 };
-
 
 const processAnswer = () => {
   let answer;
@@ -45,10 +44,18 @@ const processAnswer = () => {
   } else {
     nextProblem();
   }
+};
 
+const getRandomInts = (numOne, numTwo) => {
+  return {
+    numOne: Math.floor(Math.random() *
+    (maxValue - minValue + 1)) + minValue,
 
-
-}
+    numTwo: Math.floor(Math.random() *
+    (maxValue - minValue + 1)) + minValue
+  };
+ 
+};
 
 //Countdown function starts when 'Start' is clicked.
 startButton.onclick = function () {
@@ -74,7 +81,7 @@ startButton.onclick = function () {
       questionNumber = 1;
     }
   }, 1000);
-}
+};
 
 //Clear countdown when 'Clear' is clicked.
 clearButton.onclick = function () {
@@ -86,16 +93,35 @@ clearButton.onclick = function () {
     prompt.innerHTML = ` <p id ='start-banner'>Press Start</p>`
     questionNumber = 1;
   }
-}
+};
 
 
 //Create a function for evaluating the user input.
 function nextProblem() {
+  const { numOne, numTwo } = getRandomInts(minValue, maxValue);
+  numberOne = numOne;
+  numberTwo = numTwo;
+  solution = numOne + numTwo;
   questionNumber++;
-  //Increases values after question 5.
-  if (questionNumber > 5) {
-    minValue = 50;
-    maxValue = 75;
+  //Increases values after 3 questions.
+  if (questionNumber > 2) {
+    minValue = 11;
+    maxValue = 19;
+  } else if (questionNumber > 7) {
+    minValue = 20;
+    maxValue = 30;
+  } else if (questionNumber > 10) {
+    minValue = 31;
+    maxValue = 45;
   }
+  else if (questionNumber > 13) {
+    minValue = 46;
+    maxValue = 55;
+  } else if (questionNumber > 16) {
+    minValue = 56;
+    maxValue = 65;
+  }
+  
   handleGame();
-}
+  
+};
